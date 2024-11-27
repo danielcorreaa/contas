99d49a68dacddc9239ee39a4f361313b4a02a35d
danielcorreaa <daniel.cor@outlook.com>
2024-11-27 15:22:00 -0300

    add script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Docker Run"
+sudo apt update
+sudo apt install ca-certificates curl
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+docker --version
+
+echo "Manage the Docker System Service"
+
+sudo systemctl enable docker
+sudo systemctl status docker
+
+sudo systemctl stop docker
+
+sudo systemctl start docker
+
+sudo usermod -aG docker linuxuser
+
+docker --version
